e8fdefa1f3a5ddc519fa8419178526cf8de85244
panpengfei <panpengfei@zhangwen.cn>
2024-01-15 15:07:06 +0800

    [feature] add test file

---
+++ src/fmdb/FMTest.h
@@ -0,0 +1,18 @@
+//
+//  Test.h
+//  fmdb
+//
+//  Created by 潘鹏飞 on 2024/1/15.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FMTest : NSObject
+
++ (void)testMethod;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ src/fmdb/FMTest.m
@@ -0,0 +1,17 @@
+//
+//  Test.m
+//  fmdb
+//
+//  Created by 潘鹏飞 on 2024/1/15.
+//
+
+#import "Test.h"
+#import <Foundation/Foundation.h>
+
+@implementation FMTest
+
++ (void)testMethod {
+    NSLog(@"hello world")
+}
+
+@end
